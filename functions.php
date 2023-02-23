fbaea1ccdf3412e6b640174d5bb2aabf4f6b2070
Steve Ryan <steve.ryan@asu.edu>
2023-02-23 13:49:06 -0700

    Relocate global snapshot feature to footer

--- functions.php
+++ functions.php
@@ -74,3 +74,59 @@ function uds_furi_enable_post_thumbnails() {
 	add_theme_support( 'post-thumbnails', array( 'participant', 'furiproject' ) );
 }
 add_action( 'after_setup_theme', 'uds_furi_enable_post_thumbnails' );
+
+
+/**
+ * Rewrite of footer_action_row function from parent theme.
+ * Includes count-up feature in place of link columns.
+ */
+function pitchfork_furi_render_footer_action_row() {
+	$action_row_status = get_theme_mod( 'footer_row_actions' );
+
+	$multisite_enhance = false;
+	$multisite_enhance = get_field('pitchfork_options_enhanced_multisite', 'option');
+
+	$multisite_enhance_footer = false;
+	$multisite_enhance_footer = get_field('pitchfork_options_root_footer', 'option');
+
+	// Enhanced multisite check. Do we want to use the "root" footer links menu?
+	if( (is_multisite()) && ( $multisite_enhance ) && ( $multisite_enhance_footer ) ) {
+		// Switch our database context to the 'main' blog of our multisite.
+		switch_to_blog( get_main_site_id() );
+	}
+
+	if ( 'enabled' === $action_row_status ) {
+		?>
+		<nav aria-label="Footer">
+			<div class="container" id="footer-columns">
+				<div class="row">
+
+					<div class="col-lg-3" id="info-column">
+						<h5><span class="footer-site-name" id="footer-unit-text"><?php uds_wp_render_footer_unit_name(); ?></span></h5>
+						<div class="contact-wrapper">
+							<?php uds_wp_render_contact_link(); ?>
+						</div>
+
+						<div class="contribute-wrapper">
+							<?php uds_wp_render_contribute_button(); ?>
+						</div>
+					</div>
+					<?php
+					// include get_template_directory() . '/template-parts/asu-footer-menu.php';
+					get_template_part( 'templates/snapshot', 'footer' );
+					?>
+				</div> <!-- row -->
+			</div> <!-- footer-columns -->
+		</nav>
+		<?php
+	}
+
+	/**
+	 * Because we may have switched blog IDs earlier, switch back to the current
+	 * blog, just in case.
+	 */
+	if( is_multisite() && ms_is_switched() ) {
+		restore_current_blog();
+	}
+
+}
--- templates/snapshot-footer.php
+++ templates/snapshot-footer.php
@@ -7,12 +7,12 @@
 
 $participants = wp_count_posts( 'participant' )->publish;
 $mentors = wp_count_terms( 'faculty_mentor');
-$symposia = wp_count_terms( 'symposium_date'); 
-$focusareas = wp_count_terms( 'research_theme' );
+$symposia = wp_count_terms( 'symposium_date');
+$focusareas = wp_count_terms( 'research_theme', 'hide_empty=true' );
 
 // Pull query data for all participants, loop and total for individual terms.
 $projects = array();
-$presentations = get_terms( 
+$presentations = get_terms(
 	array(
 		'taxonomy' => 'presentation_type',
 		'hide_empty' => true,
@@ -36,70 +36,40 @@ foreach ($presentations as $presentation) {
 	$projects[$presentation->name] = $count;
 }
 ?>
-
-<section class="section-head" id="snapshot-global-title">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<h3><span class="text-gold">FURI</span> Totals</h3>
-			</div>
+<div class="col-lg-9">
+	<!-- <h3><span class="highlight-white">FURI Totals</span></h3> -->
+	<div id="snapshot-footer">
+		<div class="stat-package">
+			<h3><span>Total</span>Students</h3>
+			<div class="counter" data-count="<?php echo esc_html( $participants ); ?>">0</div>
+		</div>
+		<div class="stat-package">
+			<h3><span>Faculty</span>Mentors</h3>
+			<div class="counter" data-count="<?php echo esc_html( $mentors ); ?>">0</div>
+		</div>
+		<div class="stat-package">
+			<h3><span>Symposia</span>Archived</h3>
+			<div class="counter" data-count="<?php echo esc_html( $symposia ); ?>">0</div>
+		</div>
+		<div class="stat-package focus-areas">
+			<h3><span>Focus</span>Areas</h3>
+			<div class="counter" data-count="<?php echo esc_html( $focusareas ); ?>">0</div>
+		</div>
+		<div class="stat-package">
+			<h3><span>FURI</span>Projects</h3>
+			<div class="counter" data-count="<?php echo esc_html( $projects['FURI'] ); ?>">0</div>
+		</div>
+		<div class="stat-package">
+			<h3><span>MORE</span>Projects</h3>
+			<div class="counter" data-count="<?php echo esc_html( $projects['MORE'] ); ?>">0</div>
+		</div>
+		<!-- <div class="stat-package">
+			<h3><span>KEEN</span>Projects</h3>
+			<div class="counter" data-count="<?php echo esc_html( $projects['KEEN'] ); ?>">0</div>
+		</div> -->
+		<div class="stat-package">
+			<h3><span>GCSP</span>Projects</h3>
+			<div class="counter" data-count="<?php echo esc_html( $projects['GCSP'] ); ?>">0</div>
 		</div>
 	</div>
-</section>
-
-<section class="wrapper" id="snapshot-global">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-3">
-				<div class="stat-package d-flex flex-row">
-					<h3><span>Total</span>Students</h3>
-					<div class="counter" data-count="<?php echo esc_html( $participants ); ?>">0</div>
-				</div>
-			</div>
-			<div class="col-md-3">
-				<div class="stat-package d-flex flex-row">
-					<h3><span>Faculty</span>Mentors</h3>
-					<div class="counter" data-count="<?php echo esc_html( $mentors ); ?>">0</div>
-				</div>
-			</div>
-			<div class="col-md-3">
-				<div class="stat-package d-flex flex-row">
-					<h3><span>Online</span>Symposia</h3>
-					<div class="counter" data-count="<?php echo esc_html( $symposia ); ?>">0</div>
-				</div>
-			</div>
-			<div class="col-md-3">
-				<div class="stat-package d-flex flex-row focus-areas">
-					<h3><span>Focus</span>Areas</h3>
-					<div class="counter" data-count="<?php echo esc_html( $focusareas ); ?>">0</div>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-3">
-				<div class="stat-package d-flex flex-row">
-					<h3><span>FURI</span>Projects</h3>
-					<div class="counter" data-count="<?php echo esc_html( $projects['FURI'] ); ?>">0</div>
-				</div>
-			</div>
-			<div class="col-md-3">
-				<div class="stat-package d-flex flex-row">
-					<h3><span>MORE</span>Projects</h3>
-					<div class="counter" data-count="<?php echo esc_html( $projects['MORE'] ); ?>">0</div>
-				</div>
-			</div>
-			<div class="col-md-3">
-				<div class="stat-package d-flex flex-row">
-					<h3><span>KEEN</span>Projects</h3>
-					<div class="counter" data-count="<?php echo esc_html( $projects['KEEN'] ); ?>">0</div>
-				</div>
-			</div>
-			<div class="col-md-3">
-				<div class="stat-package d-flex flex-row">
-					<h3><span>GCSP</span>Projects</h3>
-					<div class="counter" data-count="<?php echo esc_html( $projects['GCSP'] ); ?>">0</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
+</div>
