06d82129e82f3ddd7630df2f41bc2c8f9dae4d97
Steve Ryan <steve.ryan@asu.edu>
2023-03-07 17:36:24 -0700

    Add support for modal window, QR codes.

--- js/child-theme.js
+++ js/child-theme.js
@@ -2,35 +2,45 @@
 // If you are rolling some jQuery you'll want to use a doc ready statement like the following.
 
 jQuery(document).ready(function ($) {
-    // Logic for maniuplating content within single-participant.php
-    if ($('body').hasClass('single-participant')) {
-        $('.project-icon').detach().appendTo('#project-icon-column');
-    }
+	// Logic for maniuplating content within single-participant.php
+	if ($('body').hasClass('single-participant')) {
+		$('.project-icon').detach().appendTo('#project-icon-column');
+	}
 
-    // Add tooltips to all pages if there's one present.
-    $('[data-toggle="tooltip"]').tooltip();
+	// Adding click tracking for modal window, part of the QR code feature.
+	// Code adapted from the story present in unity.web.asu.edu
+	$('.openModalButton').click(function () {
+		$(this).next().addClass('open');
+	});
 
-    // Simple countup feature for the pre-footer area.
-    $('.counter').each(function () {
-        var $this = $(this),
-            countTo = $this.attr('data-count');
+	$('.closeModalButton').click(function () {
+		$(this).closest('.uds-modal').removeClass('open');
+	});
 
-        $({ countNum: $this.text() }).animate(
-            {
-                countNum: countTo,
-            },
+	// Add tooltips to all pages if there's one present.
+	$('[data-toggle="tooltip"]').tooltip();
 
-            {
-                duration: 2000,
-                easing: 'linear',
-                step: function () {
-                    $this.text(Math.floor(this.countNum));
-                },
-                complete: function () {
-                    $this.text(this.countNum);
-                    //alert('finished');
-                },
-            }
-        );
-    });
+	// Simple countup feature for the pre-footer area.
+	$('.counter').each(function () {
+		var $this = $(this),
+			countTo = $this.attr('data-count');
+
+		$({ countNum: $this.text() }).animate(
+			{
+				countNum: countTo,
+			},
+
+			{
+				duration: 2000,
+				easing: 'linear',
+				step: function () {
+					$this.text(Math.floor(this.countNum));
+				},
+				complete: function () {
+					$this.text(this.countNum);
+					//alert('finished');
+				},
+			}
+		);
+	});
 });
