96aa64a4f9108437adc301d8d8af8acebb957bb3
Steve Ryan <steve.ryan@asu.edu>
2023-03-31 14:07:06 -0700

    FIX: Fatal error caused by faculty mentor not selected.

--- post-questions.php
+++ post-questions.php
@@ -26,48 +26,54 @@ get_header();
 			</div>
 			<div class="featured-mentor">
 				<?php
-					// Retrieve details about linked mentor.
+					// Retrieve details about a possible linked mentor.
 					$post_id = get_the_ID();
 					$mentor = get_the_terms( $post_id, 'faculty_mentor');
-					$mentorID = 'term_' . $mentor[0]->term_id;
-					$mentor_asurite = get_field( '_mentor_asurite', $mentorID );
-					if (! empty( $mentor_asurite )) {
-						$search_json = 'https://search.asu.edu/api/v1/webdir-profiles/faculty-staff/filtered?asurite_ids=' . $mentor_asurite . '&size=1&client=fse_furl';
 
-						$search_request = wp_safe_remote_get( $search_json );
+					// Produce a linked faculty mentor portrait & details if one is available.
+					if ( ! empty ( $mentor ) ) {
 
-						// Error check for invalid JSON.
-						if ( is_wp_error( $search_request ) ) {
-							return false; // Bail early.
+						$mentorID = 'term_' . $mentor[0]->term_id;
+						$mentor_asurite = get_field( '_mentor_asurite', $mentorID );
+						if (! empty( $mentor_asurite )) {
+							$search_json = 'https://search.asu.edu/api/v1/webdir-profiles/faculty-staff/filtered?asurite_ids=' . $mentor_asurite . '&size=1&client=fse_furl';
+
+							$search_request = wp_safe_remote_get( $search_json );
+
+							// Error check for invalid JSON.
+							if ( is_wp_error( $search_request ) ) {
+								return false; // Bail early.
+							}
+
+							$search_body   = wp_remote_retrieve_body( $search_request );
+							$search_data   = json_decode( $search_body );
+
+							if ( ! empty( $search_data ) ) {
+								$path = $search_data->results[0];
+								$mentor_photo = $path->photo_url->raw;
+							}
+
 						}
 
-						$search_body   = wp_remote_retrieve_body( $search_request );
-						$search_data   = json_decode( $search_body );
-
-						if ( ! empty( $search_data ) ) {
-							$path = $search_data->results[0];
-							$mentor_photo = $path->photo_url->raw;
+						// Check for featured mentor status. Should be set at this point.
+						$mentorstring = '';
+						$mentorprogram = get_field( '_mentor_featured_program', $mentorID );
+						if ( ! empty ( $mentorprogram )) {
+							$mentorstring = '<h3><span class="highlight-gold">Featured mentor, ' . esc_html( $mentorprogram->name ) . '</span></h3>';
 						}
 
-					}
+						$portrait = '';
+						// Check for featured image or Search photo
+						if (! empty( $mentor_photo ) ) {
+							$portrait = '<img class="isearch-image img-fluid" src="' . $mentor_photo . '" alt="Portrait of ' . $mentor[0]->name . '"/>';
+						}
 
-					// Check for featured mentor status. Should be set at this point.
-					$mentorstring = '';
-					$mentorprogram = get_field( '_mentor_featured_program', $mentorID );
-					if ( ! empty ( $mentorprogram )) {
-						$mentorstring = '<h3><span class="highlight-gold">Featured mentor, ' . esc_html( $mentorprogram->name ) . '</span></h3>';
-					}
+						echo $portrait;
+						echo '<div class="mentor-name"><h2>' . $mentor[0]->name . '</h2>';
+						echo $mentorstring;
+						echo '<p><a href="' . get_term_link( $mentor[0]->term_id ) . '">View mentored projects</a></p></div>';
 
-					$portrait = '';
-					// Check for featured image or Search photo
-					if (! empty( $mentor_photo ) ) {
-						$portrait = '<img class="isearch-image img-fluid" src="' . $mentor_photo . '" alt="Portrait of ' . $mentor[0]->name . '"/>';
 					}
-
-					echo $portrait;
-					echo '<div class="mentor-name"><h2>' . $mentor[0]->name . '</h2>';
-					echo $mentorstring;
-					echo '<p><a href="' . get_term_link( $mentor[0]->term_id ) . '">View mentored projects</a></p></div>';
 
 				?>
 			</div>
