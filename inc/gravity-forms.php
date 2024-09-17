dda88e357792c00a268a2bca1f4c6982b57247e6
Steve Ryan <steve.ryan@asu.edu>
2024-09-17 10:49:17 -0700

    WIP: Testing new hook for post creation.

--- inc/gravity-forms.php
+++ inc/gravity-forms.php
@@ -71,43 +71,34 @@ function furi_abstract_assign_misc_data($post_id, $feed, $entry, $form) {
 
 // Create participant_to_project connections after post submission.
 // ===============================================
-add_action( 'gform_after_submission_1', 'create_project_connections', 10, 2 );
-function create_project_connections( $entry, $form ) {
+add_action( 'gform_advancedpostcreation_post_after_creation_1', 'create_project_connections', 10, 2 );
+function create_project_connections( $post_id, $feed, $entry, $form ) {
 
-	// More than one post may be created for a form submission.
-	// The created post ids are stored as an array in the entry meta
+	// This function should run either once or twice depending on the logic of the form.
+
+	// All of the created post ids are stored as an array in the entry meta
 	$created_posts = gform_get_meta( $entry['id'], 'gravityformsadvancedpostcreation_post_id' );
 
-	// Log stuff from this submission.
-
-	$log_file = WP_CONTENT_DIR . '/gravity-forms-log.txt'; // Path to the log file
-
-	$form_id = $form['id'];
-	$entry_id = $entry['id'];
-	$created_post_string = print_r($created_posts, true);
-
-	// Format log entry
-	$log_entry = "Form ID: $form_id | Entry ID: $entry_id | Created Posts: $created_post_string " . PHP_EOL;
-
-	// Write log entry to the file
-	file_put_contents($log_file, $log_entry, FILE_APPEND);
-
-	/// Normal code from here forward.
+	// Testing if the returned value is an array. If not, we can skip everything else here.
+	if (! is_array($created_posts)) {
+		$this->log_debug( __METHOD__ . 'The $created_posts var returned a value of {$created_posts}');
+		return;
+	}
 
 	// Count the number of things in the array.
 	if ( count( $created_posts ) > 1 ) {
-		// Greater than one item in the array means it created a project & za person.
+		// Greater than one item in the array means two post types were created. A project + a person.
 		// Get both items in the array and create a connection.
 		$from = $created_posts[0]['post_id'];
 		$to = $created_posts[1]['post_id'];
 	} else {
 		// The form submission created exactly one post object. It will be a project. Get the id.
 		// Also get the value of the post ID already selected in the form. That's stored in fieldID=5
-		$from = $created_posts[0]['post_id'];
+		$from = $post_id;
 		$to = $entry[5];
 	}
 
-	// Create the connection.
+	// Create the connection within post-2-post
 	p2p_type( 'participants_to_projects' )->connect( $from, $to, array( 'date' => current_time( 'mysql' ) ) );
 
 }
